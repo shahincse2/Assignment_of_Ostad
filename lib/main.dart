641abaef1a728e1d8d7468e7a34f850cf975bc97
shahincse2 <shahinalam.cse2@gmail.com>
2024-09-20 15:14:38 +0600

    CRUD Operation with API

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
-import 'package:assignment_of_ostad/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
+  runApp(const CrudApp());
 }
